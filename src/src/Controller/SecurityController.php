f8d835be2bd2e204c2711c9d6a28b887e1d6603a
AMESSAGER <adrien.messager.diaz@bellvision.fr>
2021-01-05 11:35:54 +0100

    Security/Login Config

--- src/src/Controller/SecurityController.php
+++ src/src/Controller/SecurityController.php
@@ -33,4 +33,12 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function home(): Response
+    {
+        return $this->redirect('/login');
+    }
 }
