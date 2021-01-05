174d196a402b6ea5b6fe1890cdddc2398ceafe1c
AMESSAGER <adrien.messager.diaz@bellvision.fr>
2021-01-05 17:32:03 +0100

    Categories CRUD

--- src/assets/app.js
+++ src/assets/app.js
@@ -11,3 +11,14 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 
+//A2lix
+import a2lix_lib from '@a2lix/symfony-collection/src/a2lix_sf_collection';
+
+a2lix_lib.sfCollection.init({
+    collectionsSelector: 'form div[data-prototype]',
+    manageRemoveEntry: true,
+    lang: {
+        add: 'Add',
+        remove: 'Remove'
+    }
+})
--- src/src/Entity/Category.php
+++ src/src/Entity/Category.php
@@ -25,7 +25,7 @@ class Category
     private $name;
 
     /**
-     * @ORM\OneToMany(targetEntity=Subcategory::class, mappedBy="category")
+     * @ORM\OneToMany(targetEntity=Subcategory::class, mappedBy="category", cascade={"persist"})
      */
     private $subcategories;
 
