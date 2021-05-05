aebaa19daccfe97822127de65ab01b9a60b77def
pow3rr3d <adrien.messager.diaz@gmail.com>
2021-05-05 13:28:08 +0200

    Install Vich Uploader

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
